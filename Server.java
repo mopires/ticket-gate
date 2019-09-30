2a14ac860a0ffa0d9e0c076fccbc9ad86f2e2c68
Matheus Pires <mopires23@gmail.com>
2019-09-30 10:27:10 -0300

    Add files via upload

--- Server.java
+++ Server.java
@@ -1,10 +1,12 @@
 
 import java.io.*;
-import java.lang.reflect.Array;
 import java.net.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import org.json.*;
+import java.util.HashMap;
+
+import org.json.simple.*;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.json.simple.JSONArray;
 
 public class Server {
 
@@ -14,16 +16,16 @@ public class Server {
         serverSocket = new ServerSocket(port);
     }
 
-    private Socket Wait() throws IOException{
+    private Socket Wait() throws IOException {
         Socket socket = serverSocket.accept();
         return socket;
     }
 
-    private void ProcessRequest(Socket socket){
+    private void ProcessRequest(Socket socket) {
 
         String request, response;
 
-        try{
+        try {
             ObjectOutputStream output =
                     new ObjectOutputStream(socket.getOutputStream());
             ObjectInputStream input =
@@ -36,85 +38,117 @@ public class Server {
             output.flush();
 
 
-        }
-        catch (IOException ex){
+        } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
 
     }
 
-    private boolean IsOpen(){
+    private boolean IsOpen() {
 
-        //verifica disponibilidade
-        /*boolean[] list = GetList();
-        if (list == null) {
-            System.out.println("Error: Fail to retrieve the list of tickets");
-            return false;
-        }else{
+        //verifica disponibilidade o TAMANHO DO ARRAY SE MAIOR QUE MIL TRANCA
+        boolean[] list = GetList();
+        boolean rt = false;
 
-            for (int i = 0; i < list.length; i++){
-                if (!list[i]){
-                    System.out.println(list[i]);
-                    return true;
-                }
-                else
-                    return false;
+        for (int i = 0; i < list.length; i++) {
+
+            if (!list[i]) {
+                list[i] = true; // fill the spot
+                CheckIn(list);
+                rt = true; //return true  to allow entrance
+                break;
             }
-
-        }*/
-        return true;
+        }
+        return rt;
     }
 
     private boolean[] GetList() {
         boolean[] list = null;
         try {
 
-            InputStream file = new FileInputStream("/home/matheus/IdeaProjects/Server/src/spots.json");
-            JSONObject json = new JSONObject(file);
-            JSONArray jsonArray = json.getJSONArray("");
-            list = new boolean[jsonArray.length()];
-            for (int i = 0; i < jsonArray.length();i++){
-                list[i] = jsonArray.getBoolean(i);
+            JSONObject json;
+            //Cria o parse de tratamento
+            JSONParser parser = new JSONParser();
+
+            json = (JSONObject) parser.parse(
+                    new FileReader("/home/matheus/IdeaProjects/Server/src/spots.json"));
+
+            JSONArray jsonArray = (JSONArray) json.get("ticket");
+
+            list = new boolean[jsonArray.size()];
+
+            for (int i = 0; i < list.length; i++) {
+                list[i] = Boolean.valueOf(jsonArray.get(i).toString());
             }
 
-        }catch (FileNotFoundException ex){
+        } catch (FileNotFoundException ex) {
             System.out.println(ex.getMessage());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
         return list;
     }
 
-    /*private boolean CheckIn(String Request){
+    private void CheckIn(boolean[] list) {
 
-        //verificar o ingresso valido
+        //write on spots.json new customer
+        JSONObject json;
+        //Cria o parse de tratamento
+        JSONParser parser = new JSONParser();
 
-        return true;
-    }*/
+        try {
+
+            json = (JSONObject) parser.parse(
+                    new FileReader("/home/matheus/IdeaProjects/Server/src/spots.json"));
+
+            //arrumar aqui para escrever certo
+            HashMap<String, boolean[]> updateList = new HashMap<>();
+
+            updateList.put("ticket",list);
+
+            json.putAll(updateList);
+            JSONArray jsonArray = new JSONArray();
+            jsonArray.add(json);
 
 
-    public static void main(String argv[]){
+            FileWriter file =
+                    new FileWriter("/home/matheus/IdeaProjects/Server/src/spots.json");
+
+            file.write(jsonArray.toString());//ele escreve isso no lugar Z@5f2050f6
+            file.flush();
+
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex.getMessage());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public static void main(String argv[]) {
 
         Server server = new Server();
         try {
             server.CreateServer(5555);
-        }catch (Exception ex){
+        } catch (Exception ex) {
             System.out.println(" \n Exception: \n " + ex.getMessage());
         }
 
-            while (true){
-                try {
-                    System.out.println("Waiting connection...");
-                    Socket clientRequest = server.Wait();
-                    System.out.println("Client connected.");
-                    server.ProcessRequest(clientRequest);
-                }
-                catch (Exception ex){
-                    System.out.println(" \n Exception: \n " + ex.getMessage());
-                }
+        while (true) {
+            try {
+                System.out.println("Waiting connection...");
+                Socket clientRequest = server.Wait();
+                System.out.println("Client connected.");
+                server.ProcessRequest(clientRequest);
+            } catch (Exception ex) {
+                System.out.println(" \n Exception: \n " + ex.getMessage());
             }
-
-
-
+        }
 
 
     }
